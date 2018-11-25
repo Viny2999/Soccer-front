ee2ab927cf13a807a00a046167ef0ccacfb7d02c
Vinicius Menezes <viniciusm@oc4405682647.ibm.com>
2018-11-25 12:20:38 -0200

    create pages

---
+++ src/pages/consultar/consultar.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-consultar',
+  templateUrl: 'consultar.html',
+})
+export class ConsultarPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    
+  }
+
+}
---
+++ src/pages/editar/editar.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-editar',
+  templateUrl: 'editar.html',
+})
+export class EditarPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    
+  }
+
+}
